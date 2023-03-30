2e4654edb4927e8f6814dc2ca6700b2e9fd56d23
Douglas Martins <douglas.martins@auditornfe.com.br>
2023-03-30 08:35:43 -0300

    feat: Fazendo com que a nav incialize fechada confore o tamanho da tela

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   constructor() {}
 
+  ngOnInit(): void {
+    this.initializationNav();
+  }
+
+  // Abre e fecha a navegação lateral
   openMenu() {
     const navegacao = (document.querySelector(".nav") as HTMLElement);
     const header = (document.querySelector("header") as HTMLElement);
@@ -17,4 +22,14 @@ export class NavbarComponent {
     header.classList.toggle('moveHeader');
   }
 
+  initializationNav() {
+    const navegacao = (document.querySelector(".nav") as HTMLElement);
+    const header = (document.querySelector("header") as HTMLElement);
+    const widthWindow = window.screen.width;
+
+    if(widthWindow <= 700) {
+      navegacao.classList.toggle('openNav');
+      header.classList.toggle('moveHeader');
+    }
+  }
 }
